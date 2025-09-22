e8804d5028d48d76d6691f7cc31813f189c28b24
Anwar12052006 <anwarraza6299@gmail.com>
2025-09-23 00:50:50 +0530

    15: agent update delete

--- src/modules/agents/server/ui/views/components/agent-form.tsx
+++ src/modules/agents/server/ui/views/components/agent-form.tsx
@@ -57,6 +57,24 @@ export const AgentForm = ({
         }),
     );
 
+    const updateAgent = useMutation(
+        trpc.agents.update.mutationOptions({
+            onSuccess: async () => {
+                await queryClient.invalidateQueries(
+                    trpc.agents.getMany.queryOptions({}),
+                );
+
+                
+                onSuccess?.();
+            },
+            onError: (error) => {
+                toast.error(error.message);
+
+                //TODO: check if error is "forebidden", redirect to"/upgrade"
+            },
+        }),
+    );
+
     const form = useForm<z.infer<typeof agentsInsertSchema>>({
         resolver: zodResolver(agentsInsertSchema),
         defaultValues: {
@@ -66,11 +84,11 @@ export const AgentForm = ({
     });
 
     const isEdit = !!initialValues?.id;
-    const isPending = createAgent.isPending;
+    const isPending = createAgent.isPending || updateAgent.isPending;
 
     const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
         if (isEdit) {
-            console.log("TODO: updateAgent")
+            updateAgent.mutate({ ...values, id: initialValues.id });
         } else {
             createAgent.mutate(values);
         }
