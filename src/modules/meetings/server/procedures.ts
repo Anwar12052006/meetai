58ddf485b76922c2c3c80e7cbfee4b07c1d536f1
Anwar12052006 <anwarraza6299@gmail.com>
2025-09-25 11:07:24 +0530

    17: meetings form

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -5,8 +5,47 @@ import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { and, count, desc, eq, getTableColumns, ilike } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
+import { meetingsInsertSchema, meetingsUpdateSchema } from "./schemas";
 
 export const meetingsRouter = createTRPCRouter({
+    update: protectedProcedure
+        .input(meetingsUpdateSchema)
+        .mutation(async ({ ctx, input}) => {
+            const [updateMeeting] = await db
+            .update(meetings)
+            .set(input)
+            .where(
+                and(
+                    eq(meetings.id, input.id),
+                    eq(meetings.userId, ctx.auth.user.id),
+                )
+            )
+            .returning();
+    
+            if(!updateMeeting) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Meeting not found",
+                });
+            }
+    
+            return updateMeeting;
+        }),
+     create: protectedProcedure
+        .input(meetingsInsertSchema)
+        .mutation(async ({ input, ctx}) => {
+           const [createdMeetings] = await db
+            .insert(meetings)
+            .values({
+                ...input,
+                userId: ctx.auth.user.id,
+            })
+            .returning();
+    
+            //TODO: Create stream call, Upsert stream users
+
+            return createdMeetings;
+        }),
      getOne: protectedProcedure
      .input(z.object({id: z.string() }))
      .query(async ({ input, ctx }) => {
@@ -42,7 +81,7 @@ export const meetingsRouter = createTRPCRouter({
     .query(async ({ctx, input}) => {
         const { search, page, pageSize } = input;
 
-        
+
 
         const data = await db
         .select({
