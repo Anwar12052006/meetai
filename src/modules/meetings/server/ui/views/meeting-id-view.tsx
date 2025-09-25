c4e74550ec64945979a6db8afc3ffe805f5af7cf
Anwar12052006 <anwarraza6299@gmail.com>
2025-09-26 01:31:20 +0530

    21: meeting variants

--- src/modules/meetings/server/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/server/ui/views/meeting-id-view.tsx
@@ -6,10 +6,14 @@ import { useTRPC } from "@/trpc/client";
 import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { MeetingIdViewHeader } from "./components/meeting-id-view-header";
 import { useRouter } from "next/navigation";
-import { toast}  from "sonner";
+// import { toast}  from "sonner";
  import { useConfirm } from "@/hooks/use-confirm";
 import { UpdateMeetingDialog } from "./components/update-meeting-dialog";
 import { useState } from "react";
+import { UpcomingState } from "./components/upcoming-state";
+import { ActiveState } from "./components/active-state";
+import { CancelledState } from "./components/cancelled-state";
+import { ProcessingState } from "./components/processing-state";
 
 interface Props {
   meetingId: string;
@@ -49,6 +53,12 @@ export const MeetingIdView = ({ meetingId }: Props) => {
     await removeMeeting.mutateAsync({ id: meetingId })
   };
 
+  const isActive = data.status === "active"
+  const isUpcoming = data.status === "upcoming"
+  const isCancelled = data.status === "cancelled"
+  const isProcessing = data.status === "processing"
+
+
   return (
     <>
     <RemoveConfirmation />
@@ -57,14 +67,23 @@ export const MeetingIdView = ({ meetingId }: Props) => {
     onOpenChange={setUpdateMeetingDialogOpen}
     initialValues={data}
     />
-    <div className="flex-1 py-4 md:px-8 flex flex-col gap-y-4">
+    <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
         <MeetingIdViewHeader 
         meetingId={meetingId}
         meetingName={data.name}
         onEdit={() => setUpdateMeetingDialogOpen(true)}
         onRemove={handleRemoveMeeting}
         />
-      {JSON.stringify(data, null, 2)}
+      {isCancelled && <CancelledState />}
+      {isProcessing && <ProcessingState />}
+      {isActive && <ActiveState meetingId={meetingId} />}
+      {isUpcoming && (
+        <UpcomingState 
+          meetingId={meetingId}
+          onCancelMeeting={() => {}}
+          isCancelling={false}
+        />
+      )}
     </div>
     </>
   );
